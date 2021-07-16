15a418444ce99deef5f2710647293a37454f7d1b
huchzi <huchzi@email.de>
2021-07-16 16:05:35 +0200

    Imported function for plotting tcsf curves.

---
+++ R/show_plot.R
@@ -0,0 +1,12 @@
+show_plot <- function (t) {
+  ggplot(t, aes(x = frequency, y = sensitivity, color = stimulus_type,
+                group = date_of_exam)) +
+    geom_point() +
+    geom_line() +
+    scale_x_log10() +
+    scale_y_log10() +
+    scale_color_manual(values = c("L-cone" = "red",
+                                  "M-cone" = "green",
+                                  "S-cone" = "blue",
+                                  "Rod" = "orange"))
+}
