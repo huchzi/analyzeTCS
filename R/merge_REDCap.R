b58f99362130b2a07ca547babac1ccb8754e56e0
huchzi <huchzi@email.de>
2021-08-12 13:51:08 +0200

    Added function for merging REDCap data to tCSF.

---
+++ R/merge_REDCap.R
@@ -0,0 +1,28 @@
+mergeREDCap <- function(tCSF, redcap) {
+  
+  stopifnot(is.data.table(tCSF))
+  stopifnot(nrow(tCSF) > 0)
+  
+  redcap[end_date == "", end_date := date_of_exam]
+  
+  exams <- split(redcap, 1:nrow(redcap))
+  exams <- 
+    rbindlist(
+      lapply(exams, 
+             function(x) data.table(patid = as.integer(x$patid),
+                                    redcap_repeat_instance = as.integer(x$redcap_repeat_instance),
+                                    date_of_exam = seq(as.Date(x$date_of_exam), 
+                                                       as.Date(x$end_date),
+                                                       by = "days")
+             )
+      )
+    )
+  
+  data <- merge(redcap[, -"date_of_exam"], exams, 
+                by = c("patid", "redcap_repeat_instance"), 
+                all.y = TRUE)
+  
+  result <- merge(tCSF, redcap, by = c("patid", "date_of_exam"))
+  
+  result
+}
