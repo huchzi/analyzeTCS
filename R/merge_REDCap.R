e4f001886804d730311656311774f5d25201c9d8
huchzi <huchzi@email.de>
2021-08-12 15:10:38 +0200

    Update merge_REDCap.R

--- R/merge_REDCap.R
+++ R/merge_REDCap.R
@@ -3,7 +3,7 @@ merge_redcap <- function(tCSF, redcap) {
   stopifnot(is.data.table(tCSF))
   stopifnot(nrow(tCSF) > 0)
   
-  redcap[end_date == "", end_date := date_of_exam]
+  redcap[end_date == "", end_date := begin_date]
   
   exams <- split(redcap, 1:nrow(redcap))
   exams <- 
@@ -11,14 +11,14 @@ merge_redcap <- function(tCSF, redcap) {
       lapply(exams, 
              function(x) data.table(patid = as.integer(x$patid),
                                     redcap_repeat_instance = as.integer(x$redcap_repeat_instance),
-                                    date_of_exam = seq(as.Date(x$date_of_exam), 
+                                    date_of_exam = seq(as.Date(x$begin_date), 
                                                        as.Date(x$end_date),
                                                        by = "days")
              )
       )
     )
   
-  data <- merge(redcap[, -"date_of_exam"], exams, 
+  data <- merge(redcap, exams, 
                 by = c("patid", "redcap_repeat_instance"), 
                 all.y = TRUE)
   
