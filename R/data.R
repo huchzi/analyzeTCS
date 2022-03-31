55bb0474bff89d79fde82958b2dff5fa9f1e321f
huchzi <huchzi@email.de>
2022-03-31 13:05:23 +0200

    Added description of norm values.

--- R/data.R
+++ R/data.R
@@ -12,3 +12,18 @@
 #' }
 "D15_vectors"
 
+#' Norm values [dB]
+#'
+#' The norm values for photoreceptor-specific temporal contrast sensitivity [dB] for a 40-year old subject. These were re-calculated from the data published in TVST: Huchzermeyer et. al. Photoreceptor-Specific Loss of Perifoveal Temporal Contrast Sensitivity in Retinitis Pigmentosa. Transl Vis Sci Technol 2020:9(6):27.
+#'
+#' @format A data table with 36 rows and 7 variables:
+#' \describe{
+#'   \item{type}{Photoreceptor type}
+#'   \item{frequency}{Temporal frequency [Hz]}
+#'   \item{n}{Number of observations}
+#'   \item{mean_dB}{Normal sensitivity [dB] for a 40-year-old subject}
+#'   \item{sd_dB}{Standard deviation of sensitivities [dB]}
+#'   \item{confint95_left}{Lower limit of the 95% confidence interval [dB]}
+#'   \item{confint95_right}{Upper limit of the 95% confidence interval [dB]}
+#' }
+"norm_values"
